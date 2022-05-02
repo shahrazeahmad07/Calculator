f2d15dece0b05698ba8742fc6a64bd798d04dca0
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2022-05-02 12:35:35 +0500

    Decimal Point Functionality added.

--- app/src/main/java/com/uet/calculator/MainActivity.kt
+++ app/src/main/java/com/uet/calculator/MainActivity.kt
@@ -2,10 +2,41 @@ package com.uet.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvInput: TextView
+    private var lastDigit = false
+    private var lastDot = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
+
+    }
+
+    fun onDigit(view: View) {
+        tvInput.append((view as Button).text)
+        lastDigit = true
+        lastDot = false
+    }
+
+    fun onClear(view: View) {
+        tvInput.text = ""
+        lastDot = false
+        lastDigit = false
+    }
+
+    fun onDecimalPoint(view: View) {
+        if (lastDigit && !lastDot) {
+            tvInput.append(".")
+            lastDigit = false
+            lastDot = true
+        }
     }
 }
